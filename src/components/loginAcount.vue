f2b46e315e1d165c92983cc30dcf37bb737f85a8
Thanh17082001 <thienthanh17082001@gmail.com>
2023-06-30 11:16:43 +0700

    login with gg using vue

--- src/components/loginAcount.vue
+++ src/components/loginAcount.vue
@@ -1,20 +1,10 @@
 <template>
   <!-- <button @click="loginWithGoogle">Đăng nhập với GG</button> -->
-  <a href="http://localhost:3000/user/google">gg</a>
+  <a href="http://localhost:3000/user/google">Đăng nhập với Google</a>
 </template>
 
 <script>
 export default {
-  methods:{
-    async loginWithGoogle(){
-      try {
-        window.location.href = 'http://localhost:3000/user/google';
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    
-  },
   
 }
 </script>
